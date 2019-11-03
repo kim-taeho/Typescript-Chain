006ea3c31c0abef6c0ac3e1c57c7b71528b9b538
kimtaeho <xxxxoghxxx@naver.com>
2019-11-03 23:15:39 +0900

    fourth

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,43 @@
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age: number, gender: string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+import * as CryptoJS from "crypto-js";
+
+class Block {
+    public index:number;
+    public hash: string;
+    private previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    static calculateBlockHash = (index: number, previousHash:string, timestamp:number, data:string): string =>
+        CryptoJS.SHA256(index + previousHash + timestamp+ data).toString();
+
+    constructor(index: number, hash: string, previousHash:string, data:string, timestamp:number) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
+};
+
+const genesisBlock:Block = new Block(0, "12314123", "", "hello", 31232);
+
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = ():Block[] => blockchain;
+
+const getLatestBlock = ():Block => blockchain[blockchain.length - 1];
+
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data:string):Block => {
+    const previousBlock:Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimestamp:number = getNewTimestamp();
+    const nextHash:string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock: Block = new Block(newIndex, nextHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
 }
 
-const lynn = new Human("Lynn", 18, "female");
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
 
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-  };
-  
-  console.log(sayHi(lynn));
-  
-  export {};
+export {};
